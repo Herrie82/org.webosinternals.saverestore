b005e7375b077beef1be5563ab46003be470b500
Rod Whitby <rod@whitby.id.au>
2010-04-11 23:03:46 +0930

    Applications: AmpachPre, Stock Kernel (Palm Pre), Uber-Kernel (Palm Pre)

--- app/assistants/startup-assistant.js
+++ app/assistants/startup-assistant.js
@@ -9,7 +9,7 @@ function StartupAssistant()
     this.newMessages =
 	[
 	 // Don't forget the comma on all but the last entry
-	 { version: '0.6.7', log: [ 'Applications: AmpachPre' ] },
+	 { version: '0.6.7', log: [ 'Applications: AmpachPre, Stock Kernel (Palm Pre), Uber-Kernel (Palm Pre)' ] },
 	 { version: '0.6.6', log: [ 'Applications: NFL 2010, Real Soccer 2010, Sandstorm, Let\'s Golf, Shopping List, ShopList, ClassTracker, Fliq Notes' ] },
 	 { version: '0.6.5', log: [ 'Applications: NewsRoom, SecuStore' ] },
 	 { version: '0.6.4', log: [ 'Applications: Checkbook, Brothers in Arms' ] },
