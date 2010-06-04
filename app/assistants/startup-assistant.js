f0f6e66b02de0ff50a12a7f37533f49222a1ef62
Rod Whitby <rod@whitby.id.au>
2010-06-04 12:09:56 +0930

    Removed DOS line endings from some contributed scripts that caused startup failure

--- app/assistants/startup-assistant.js
+++ app/assistants/startup-assistant.js
@@ -9,6 +9,7 @@ function StartupAssistant()
     this.newMessages =
 	[
 	 // Don't forget the comma on all but the last entry
+	 { version: '0.8.1', log: [ 'Removed DOS line endings from some contributed scripts that caused startup failure' ] },
 	 { version: '0.8.0', log: [ 'Applications: Sports Live, Football Live, US Soccer Live (all courtesy of MoreSolutions)' ] },
 	 { version: '0.7.9', log: [ 'Applications: Hockey Live, Basketball Live, Baseball Live (all courtesy of MoreSolutions)' ] },
 	 { version: '0.7.8', log: [ 'Applications: Pack n Track, Pribbage, Top Stocks, Roll Quest, The Helicopter Game, Whendle, Bubble Puzzle (all courtesy of Audermars02), Time Tracker Full (courtesy of baldric)' ] },
