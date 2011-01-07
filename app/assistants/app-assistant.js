63b5833045ad053c6525dcc2346b3417a8a52a61
Rod Whitby <rod@whitby.id.au>
2011-01-07 18:04:04 +1030

    Reschedule the autosave on each run, and allow for one-shot

--- app/assistants/app-assistant.js
+++ app/assistants/app-assistant.js
@@ -23,9 +23,9 @@ AppAssistant.prototype.handleLaunch = function(params)
 							this.launchFirstScene.bind(this));
 	    }
 	} else {
-	  if(params.action === "autoSave") {
-	    this.executeAutoSave();
-	  }
+	    if (params.action === "autoSave") {
+		this.executeAutoSave();
+	    }
 	}
     }
     catch (e) {
@@ -34,74 +34,85 @@ AppAssistant.prototype.handleLaunch = function(params)
 };
 
 AppAssistant.prototype.executeAutoSave = function() {
-  if(appDB.loadedApps) {
-    this.saveApps();
-  } else {
-    appDB.initApps(this.saveApps.bind(this));
-  }
+
+    var p = prefs.get(true);
+    if (p.autoSave && (p.autoSaveFrequency > 0)) {
+	this.scheduleAutoSave();
+    }
+    
+    if (appDB.loadedApps) {
+	this.saveApps();
+    } else {
+	appDB.initApps(this.saveApps.bind(this));
+    }
 };
 
 AppAssistant.prototype.saveApps = function(e) {
   
-  if(e && e.returnValue === false) {
-    Mojo.Log.error("error saving app=%j",e);
-  }
+    if (e && e.returnValue === false) {
+	Mojo.Log.error("error saving app=%j",e);
+    }
           
-  if(!this.autoSaveApps) {
-    this.autoSaveApps = appDB.appsSaved;
-  }
+    if (!this.autoSaveApps) {
+	this.autoSaveApps = appDB.appsSaved;
+    }
   
-  if(this.autoSaveApps.length < 1) return;
+    if (this.autoSaveApps.length < 1) {
+	return;
+    }
   
-  var a = this.autoSaveApps.pop();
-  SaveRestoreService.save( this.saveApps.bind(this), a );
+    var a = this.autoSaveApps.pop();
+    if (this.subscription) {
+	this.subscription.cancel();
+    }
+    this.subscription = SaveRestoreService.save( this.saveApps.bind(this), a );
 };
 
 AppAssistant.prototype.cancelAutoSave = function() {
   
-  new Mojo.Service.Request("palm://com.palm.power/timeout", {
-    method: "clear",
-    parameters: {
-      "key": "org.webosinternals.saverestore.autoSave",
-      "uri": "palm://com.palm.applicationManager/launch",
-    },
-	onSuccess:function() { Mojo.Log.info("autoSave cancelled"); }
-  });
+    new Mojo.Service.Request("palm://com.palm.power/timeout", {
+	    method: "clear",
+	    parameters: {
+		"key": "org.webosinternals.saverestore.autoSave",
+		"uri": "palm://com.palm.applicationManager/launch",
+	    },
+	    onSuccess:function() { Mojo.Log.info("autoSave cancelled"); }
+	});
 };
 
 AppAssistant.prototype.scheduleAutoSave = function() {
   
-  var p = prefs.get(true);
+    var p = prefs.get(true);
   
-  if(!p.autoSave) return;
+    if (!p.autoSave) return;
   
-  try {
-    var when = new Date();
-    var t = new Date(p.autoSaveTime);
-    when.setHours(t.getHours());
-    when.setMinutes(t.getMinutes());
-    when.setDate(when.getDate()+parseInt(p.autoSaveFrequency))
+    try {
+	var when = new Date();
+	var t = new Date(p.autoSaveTime);
+	when.setHours(t.getHours());
+	when.setMinutes(t.getMinutes());
+	when.setDate(when.getDate()+parseInt(p.autoSaveFrequency))
     
-    var at = (when.getUTCMonth()+1)+"/"+when.getUTCDate()+"/"+when.getUTCFullYear()+" "+when.getUTCHours()+":"+when.getUTCMinutes()+":00";
+	    var at = (when.getUTCMonth()+1)+"/"+when.getUTCDate()+"/"+when.getUTCFullYear()+" "+when.getUTCHours()+":"+when.getUTCMinutes()+":00";
 
-    new Mojo.Service.Request("palm://com.palm.power/timeout", {
-      method: "set",
-      parameters: {
-        "wakeup": false,
-        "key": "org.webosinternals.saverestore.autoSave",
-        "uri": "palm://com.palm.applicationManager/launch",
-        "params": '{"id":"' + Mojo.appInfo.id + '","params":{"action":"autoSave"}}',
-        "at": at
-      },
-	  onSuccess:function() { Mojo.Log.info("autoSave scheduled"); },
-      onFailure: function(e) { Mojo.Log.error("alarm failed %j",e); }
-    });
-  } catch(e) {
-    Mojo.Log.logException(e, "AppAssistant#scheduleAutoSave");
-  }
+	new Mojo.Service.Request("palm://com.palm.power/timeout", {
+		method: "set",
+		    parameters: {
+		    "wakeup": false,
+			"key": "org.webosinternals.saverestore.autoSave",
+			"uri": "palm://com.palm.applicationManager/launch",
+			"params": '{"id":"' + Mojo.appInfo.id + '","params":{"action":"autoSave"}}',
+			"at": at
+			},
+		    onSuccess:function() { Mojo.Log.info("autoSave scheduled"); },
+		    onFailure: function(e) { Mojo.Log.error("alarm failed %j",e); }
+	});
+    } catch(e) {
+	Mojo.Log.logException(e, "AppAssistant#scheduleAutoSave");
+    }
 }
 
-AppAssistant.prototype.launchFirstScene = function(controller)
+    AppAssistant.prototype.launchFirstScene = function(controller)
 {
     vers.init();
     if (vers.showStartupScene()) {
