a9e5fd1058ef6a8312f5668a4c979ce3977a72d9
Adam Marks <asm_public-pre@yahoo.com>
2011-09-18 15:11:25 -0400

    Application: Angry Birds Seasons HD

--- app/assistants/startup-assistant.js
+++ app/assistants/startup-assistant.js
@@ -11,6 +11,7 @@ function StartupAssistant(changelog)
     this.newMessages =
 	[
 	 // Don't forget the comma on all but the last entry
+	 { version: '1.5.5', log: [ 'Application: Angry Birds Seasons HD (courtesy of Audemars02)' ] },
 	 { version: '1.5.4', log: [ 'Applications: Launcher (3.0 update), Beyond Ynth, Sparkle HD, Quell HD (all courtesy of Audemars02), Blocked (courtesy of stickb0y)' ] },
 	 { version: '1.5.3', log: [ 'Added AppNote for Robotek HD script to indicate restore only works with the same device' ] },
 	 { version: '1.5.2', log: [ 'Applications: intervalGym, Jamwam Jog, Power Nap, ZenSearch (all courtesy of irateb), Need For Speed Hot Pursuit (TouchPad) (courtesy of A.Stice), FlightView, Gowalla, Open Table, Robotek HD, Slice It HD, Spare Time, Wall Switch (all courtesy of Audemars02)' ] },
